855040fbd2f6811bfada76945ae77d90fc2d97e8
Michael <michael@teuluseren.com>
2020-11-11 22:31:57 -0500

    Revert "Raffle"

--- src/main/java/com/gso/jug/controller/MainController.java
+++ src/main/java/com/gso/jug/controller/MainController.java
@@ -1,20 +1,11 @@
 package com.gso.jug.controller;
 
-import com.gso.jug.model.Raffle;
-import com.gso.jug.repository.RaffleRepository;
-import com.gso.jug.service.RaffleService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequiredArgsConstructor
 public class MainController {
-
-	private final RaffleRepository raffleRepository;
-	private final RaffleService raffleService;
 	
 	@GetMapping("/")
 	public String getDashBoard(Model model) {
@@ -30,21 +21,5 @@ public class MainController {
 	public String getLocationPage(Model model) {
 		return "location-page/location-main";
 	}
-
-	@PostMapping(value = "/raffle" , consumes = MediaType.APPLICATION_JSON_VALUE , produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody Long enterRaffle(@RequestBody Raffle raffle) {
-		Raffle exisiting = raffleRepository.findByEmail(raffle.getEmail().trim());
-
-		if(null != exisiting) {
-			return exisiting.getId();
-		}
-
-		return raffleRepository.save(raffle).getId();
-	}
-
-	@GetMapping(value = "/draw", produces = MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody Raffle draw(){
-	  return raffleService.draw();
-	}
-
+	
 }
